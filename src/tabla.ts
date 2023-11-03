e27c112b50001fc6ec03733e19042f4ea22973d2
Ju4nJ023 <jjaf2000@gmail.com>
2023-11-03 15:38:48 +0100

    mensaje

---
+++ src/tabla.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+console.log("%cThe Beetles","background-color:green; font-size:15px; font-weight:bold;","/1960","/Activo:true","/🎵 Pop Rock");
+console.log("%cQueen","background-color:green; font-size: 15px; font-weight:bold;","/1970","/Activo:false","/🎸  Pop Rock");
+console.log("%cAC DC","background-color:green; font-size: 15px; font-weight:bold;","/1973","/Activo:true","/🤘 Hard Rock");
+console.log("%cLudwig van Beethoven","background-color:green; font-size: 15px; font-weight:bold;","/1770","/Activo:false","/🎼 Pop Rock");
+console.log("%cThe rolling stones","background-color:green; font-size: 15px; font-weight:bold;","/1962","/Activo:true","/🎸 Pop Rock");
+
+
+
